19353335b24ce873e5cba770c134f19d34f0ef41
joker <ziyuexuanmail@gmail.com>
2016-06-15 17:57:04 +0800

    update ddns for test

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var https=require('https');
 
 var app=express();
 app.listen(2016,'0.0.0.0',function(){
@@ -9,4 +10,16 @@ app.use('/',express.static('../client'));
 
 app.get('/ip',function(req,res){
    res.send(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+});
+
+app.all('/updatedns/*',function(req,res){
+    var clientip=req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    https.get("https://DV8rpkNWXG5Vs8yH:wFlTwcJdEvd1whJf@domains.google.com/nic/update?hostname=dy.skymom.net&myip="+clientip,function(resp){
+        var resData = '';
+        resp.on('data',function(data){
+            resData+=data;
+        }).on('end',function(){
+            res.send(resData);
+        })
+    });
 });
